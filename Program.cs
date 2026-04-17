c9cd4e3b8bd904ae85c7b7a415cdaa3a2172ad99
Riley Beck <beckriley515@gmail.com>
2026-04-17 16:29:50 -0600

    Added comments.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using dotenv.net;
+using Fleet.Models;
 
-
+//Load in the Environment
 DotEnv.Load();
 
-string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
-	?? throw new Exception("Missing CONNECTION_STRING");
-
-
+//Set up the app for the routes.
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
  
+//Testing the database insert. Delete later.
+//
+Vehicle vehicle = new Vehicle("name")
